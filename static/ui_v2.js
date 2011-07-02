0e04e7675ad892e591f365557b311d2e0ae3e947
Mikey Levine <mrlevine@gmail.com>
2011-07-02 16:42:57 -0700

    animate things, but im not happy with it. still need to make the label transitions make sense, as they just kind of "jump" now. And maybe shrink the bars down while the slide happens, then grow them up?

--- static/ui_v2.js
+++ static/ui_v2.js
@@ -3,8 +3,9 @@ function GraphMaker(dataJson) {
 };
 
 GraphMaker.prototype.barForKey = function(key, opt_zoomKey) {
+  var self = this;
   var barElem = $('<span class="bar" />');
-  var keyData = this.data_[key];
+  var keyData = self.data_[key];
   if (keyData) {
     var cssData = {
       'background-color': keyData.rgb,
@@ -16,10 +17,37 @@ GraphMaker.prototype.barForKey = function(key, opt_zoomKey) {
     }
     if (keyData.children || opt_zoomKey) {
       var zoomKey = opt_zoomKey || key;
-      var boundGraphFn = bind(this, this.graphForKey)
+      var boundGraphFn = bind(self, self.graphForKey)
       barElem.click(function() {
+        $('.bar .value').each(function(index) {
+          if (this != barElem.children('.value').get(0)) {
+            $(this).css({'height': 1});
+          }
+        });
         var newContent = boundGraphFn(zoomKey);
-        $('#graph').replaceWith(newContent)
+        var titleElem = $('.bargraph.title .bar');
+        var curLeft = barElem.position().left;
+        var targetLeft = titleElem.position().left;
+        barElem.css({'z-index': 5});
+        if (curLeft != targetLeft) {
+          barElem.animate({
+            'left': '-=' + (curLeft - targetLeft),
+            'width': titleElem.css('width'),
+          }, 1000, function() {
+            $('#graph').replaceWith(newContent)
+          });
+        } else {
+          var parentData = self.data_[keyData.parent];
+          var childIndex = $.inArray(key, parentData.children);
+          var numChildren = parentData.children.length;
+          targetLeft = barElem.outerWidth(true) + (300 / numChildren) * childIndex;
+          barElem.animate({
+            'left': '+=' + (targetLeft - curLeft),
+            'width': 300 / numChildren,
+          }, 1000, function() {
+            $('#graph').replaceWith(newContent)
+          });
+        }
       });
     }
   }
