f4691697461afc20b8106f7f63adee5ef9e46aba
Mikey Levine <mrlevine@gmail.com>
2011-07-05 11:58:20 -0700

    fix gen script bug

--- ui_v2.py
+++ ui_v2.py
@@ -248,11 +248,11 @@ class HtmlPrinter:
       _gaq.push(['_setAccount', 'UA-24357809-1']);
       _gaq.push(['_trackPageview']);
 
-      (function() {
+      (function() {{
         var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
         ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
         var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-      })();
+      }})();
     </script>
   </head>\n'''.format(rgbs_json=rgbs_json)
 
